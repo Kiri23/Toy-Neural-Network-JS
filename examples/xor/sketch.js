6ce51cb6a976d0f685237d2f9a467515ab6b7a4a
Christian Nogueras <Christiannogueras@MBPdeChristian.lan>
2018-02-09 13:08:59 -0400

    subir archvios modificiados pero no predice bien

--- examples/xor/sketch.js
+++ examples/xor/sketch.js
@@ -13,42 +13,18 @@ let training_data = [{
   targets: [0]
 }];
 
-let lr_slider;
+nn = new NeuralNetwork(2,2,1)
+// nn.setLearningRate(0.50);
 
-function setup() {
-  createCanvas(400, 400);
-  nn = new NeuralNetwork(2, 2, 1);
-  lr_slider = createSlider(0.01, 0.1, 0.05, 0.01);
+function setup(){
+  for (let index = 0; index < 50000; index++) {
+      // choose a random input 
+      let data = random(training_data)
+      nn.train(data.inputs,data.targets)    
+  }
 }
 
-function draw() {
-  background(0);
-
-  nn.learning_rate = lr_slider.value();
-
-  for (let i = 0; i < 5000; i++) {
-    let data = random(training_data);
-    nn.train(data.inputs, data.targets);
-  }
-
-  let resolution = 20;
-  let cols = floor(width / resolution);
-  let rows = floor(height / resolution);
-
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      let x = i * resolution;
-      let y = j * resolution;
-      let input_1 = i / (cols - 1);
-      let input_2 = j / (rows - 1);
-      let output = nn.predict([input_1, input_2]);
-      let col = output[0] * 255;
-      fill(col);
-      noStroke();
-      rect(x, y, resolution, resolution);
-    }
-  }
-
-
-
-}
+console.log(nn.predict([1,0]) );
+console.log(nn.predict([0,1]) );
+console.log(nn.predict([0,0]) );
+console.log(nn.predict([1,1]) );
